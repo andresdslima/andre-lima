130265b40fe3d11a6bddb9b04b9f2b9eed907325
Andre Lima <andresdslima@gmail.com>
2024-11-22 22:24:28 +0000

    feature: initial code base

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1 className="text-white">Andre Lima&apos;s Portfolio</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
