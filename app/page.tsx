88e7d12231251501e34a3dcd7804dae5ed2616ac
Andre Lima <andresdslima@gmail.com>
2024-11-22 23:13:28 +0000

    feature: hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Hero from "@/components/Hero";
+
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-white">Andre Lima&apos;s Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
